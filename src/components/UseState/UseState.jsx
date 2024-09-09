f5ae34df9236a65c65de9e6b6d4c784e665b77ab
Kim Tran Do <Kim.Tran-Do@metropolia.fi>
2024-09-09 12:06:21 +0300

    all test passed

--- src/components/UseState/UseState.jsx
+++ src/components/UseState/UseState.jsx
@@ -13,7 +13,7 @@ const UseState = () => {
       <h1>UseState Component</h1>
       <button onClick={() => setTheme("dark")}>Dark</button>
       <button onClick={() => setTheme("light")}>Light</button>
-      <button onClick={toggleTheme}>Toggle theme</button>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <h2>{count}</h2>
       <button onClick={() => setCount((c) => c + 1)}>Increment</button>
       <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
